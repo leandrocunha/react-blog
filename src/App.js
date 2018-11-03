7864a0fbaa024636a1eafa935fbc32421d6cb394
Leandro Cunha <leandroscunha@gmail.com>
2018-11-03 09:12:48 -0300

    chore: Add Styled-Components and Styled-Reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <GlobalStyle />
         <h1>Hello, world!</h1>
       </div>
     );
